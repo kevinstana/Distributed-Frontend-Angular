02b6ac8f3301f7accd00b7078f834855692ebaa0
Kevin Stana <it21774@hua.gr>
2024-04-09 20:39:05 +0300

    proxy test

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/';
+const AUTH_API = 'http://192.168.56.111:9090/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
